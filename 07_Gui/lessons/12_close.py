3119f28a1dda5cb72e1867614ceea802a3a72b5d
Oliver Hofmann <oliver.hofmann@th-nuernberg.de>
2024-12-11 15:33:42 +0100

    CloseApp

--- 07_Gui/lessons/12_close.py
+++ 07_Gui/lessons/12_close.py
@@ -4,6 +4,15 @@ from app_window import AppWindow
 
 class CloseApp(AppWindow):
 
+    def __init__(self, width, height, title="AppWindow"):
+        super().__init__(width, height, title)
+        screen_width = self.root.winfo_screenwidth()
+        screen_height = self.root.winfo_screenheight()
+        x = (screen_width - width) // 2
+        y = (screen_height - height) // 2
+        self.root.geometry(f"{width}x{height}+{x}+{y}")
+
+
     def on_button_click(self):
         self.root.destroy()
 
